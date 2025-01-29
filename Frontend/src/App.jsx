47d2c8cc15e0fa974b18bce322d4ef7562dae4f2
anil reddy <lokaanilreddy5@gmail.com>
2025-01-29 13:04:58 +0530

    changes

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { Login } from './Component/Login'
+import { Signup } from './Component/Signup'
+import { Home } from './page/Home'
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Login" element={<Login/>}/>
+      <Route path="/Signup" element={<Signup/>}/>
+    </Routes>
+    </>
+    
+  )
+}
+
+export default App
