814e5a87625773253cf06211642ae8f9392fa501
anil reddy <lokaanilreddy5@gmail.com>
2025-02-17 14:24:19 +0530

    changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,31 +1,25 @@
-import './App.css'
-import { Route, Router, Routes } from 'react-router-dom'
-import { Login } from './Component/Login'
-import { Signup } from './Component/Signup'
-import { Home } from './page/Home'
-import { Productform } from './Component/productform'
-import { Productcardseller } from './Component/productcardforseller'
-// import {Navbar} from './Component/Navbar'
-import Navbar from './Component/Navbar'
-
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './Components/Login';
+import { Signup } from './Components/Signup';
+import { Home } from './page/home';
+import { ProductForm } from './Components/ProductForm';
+import Navbar from '../src/Components/Navabar';
+import { Productcard } from './Components/Productcard';
 
 function App() {
   return (
-<Router>
-<Navbar/>
-<Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/Login" element={<Login/>}/>
-      <Route path="/Signup" element={<Signup/>}/>
-    <Route path="/productform" element={<Productform />} />
-    <Route path='/my-product' element={<Productcardseller/>}/>
-
-    </Routes>
-</Router>
-
-    
-    
-  )
+    <>
+    <Navbar/>
+      <Routes>
+        <Route path="/Login" element={<Login />} ></Route>
+        <Route path="/Signup" element={<Signup />} ></Route>
+        <Route path="/home" element={<Home />} ></Route>
+        <Route path='/productform' element={<ProductForm/>}/>
+        <Route path ='/productCard' element={<Productcard/>}/>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
