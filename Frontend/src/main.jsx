bfab22da7b23062f60032b63f05e8c8fd388232b
anil reddy <lokaanilreddy5@gmail.com>
2025-03-21 11:43:12 +0530

    changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import store from './store/store'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <BrowserRouter>
   <StrictMode>
     <App />
   </StrictMode>,
   </BrowserRouter>
+  </Provider>
   
 )
